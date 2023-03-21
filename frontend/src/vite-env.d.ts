3f4727fd99388b07a1b7b84ec6fa3808becf4516
Alex Geis <siegxela@gmail.com>
2023-03-21 11:11:50 -0600

    clean up modal code and move type aliases to vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,8 +1,20 @@
 /// <reference types="vite/client" />
 
+type TabListProps = {
+	viewCompleted: boolean;
+	displayCompleted: (isDisplayed: boolean) => void;
+};
+
 type TaskItem = {
 	id?: number | undefined;
 	title: string;
 	description: string;
 	completed: boolean;
+};
+
+type ModalProps = {
+	activeItem: TaskItem;
+	setActiveItem: (newActiveTask: TaskItem) => void;
+	toggle: () => void;
+	onSave: (newTask: TaskItem) => void;
 };
